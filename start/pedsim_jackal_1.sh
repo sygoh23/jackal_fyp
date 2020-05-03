6905237277706c4da0ce0063bad90b30ecfb7274
Samuel <sygoh23@student.monash.edu>
2020-05-03 20:32:32 +1000

    Combining simulations for Jackal and pedestrians.

---
+++ start/pedsim_jackal_1.sh
@@ -0,0 +1,3 @@
+source /opt/ros/melodic/setup.bash
+source ../devel/setup.bash
+roslaunch development jackal_spawn.launch
---
+++ start/pedsim_jackal_2.sh
@@ -0,0 +1,3 @@
+source /opt/ros/melodic/setup.bash
+source ../devel/setup.bash
+roslaunch development pedsim_viz.launch
