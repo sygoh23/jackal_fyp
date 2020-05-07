916cc539e9e8b38b1e57be7ed4aacf563414ce1d
Samuel <sygoh23@student.monash.edu>
2020-05-07 14:39:49 +1000

    Implemented navigation packages and file system restructure.

--- start/jackal_pedsim.sh
+++ start/jackal_pedsim.sh
@@ -3,4 +3,4 @@ sudo killall gzserver
 sudo killall gzclient
 source /opt/ros/melodic/setup.bash
 source ../devel/setup.bash
-roslaunch development full_simulation.launch
+roslaunch development jackal_pedsim.launch
--- start/pedsim_jackal_2.sh
+++
@@ -1,3 +0,0 @@
-source /opt/ros/melodic/setup.bash
-source ../devel/setup.bash
-roslaunch development pedsim_viz.launch
