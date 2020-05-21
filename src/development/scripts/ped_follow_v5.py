db105fcbb66bb017db12c4eff8669e3a45576a9f
Samuel <sygoh23@student.monash.edu>
2020-05-21 14:41:44 +1000

    Updated navigation algorithm.

--- src/development/scripts/ped_follow_v5.py
+++ src/development/scripts/ped_follow_v5.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# V4 Follows pedestrians based on distance and velocity.
+# V5: Follows pedestrians based on distance and velocity.
 import rospy
 import random
 import time
@@ -18,7 +18,7 @@ def movebase_client():
     # Set final destination:
     # Building 1: (54,39)
     # Building 2: (54,-21)
-    target_xy = [54, -21]
+    target_xy = [54, 39]
 
     # Determine number of pedestrians:
     ped_init = rospy.wait_for_message("/pedsim_simulator/simulated_agents", AgentStates)
@@ -26,9 +26,12 @@ def movebase_client():
 
     # Following parameters:
     n_loop = 200 # Number of following iterations
-    t_delay = 2 # Time between iterations (lower = more responsive)
+    t_delay = 1 # Time between iterations (lower = more responsive)
     print("Delay: %d | Iterations: %d | Target: (%d, %d)" % (t_delay, n_loop, target_xy[0], target_xy[1]))
     dist_last = [] # Last reported distance
+    dist_ped_target_last = 100000 # Last distance from robot to target
+    send_goal = 0
+    reached_target = 0
 
     for i in range(n_loop):
         print("\n\n------------------------- i = %d -------------------------" % i)
@@ -51,44 +54,52 @@ def movebase_client():
         dist_index = np.argsort(dist) # Index of sorted distance (ascending)
         vel_sort = sorted(vel) # Sort velocity (ascending)
         vel_index = np.argsort(vel) # Index of sorted velocity (ascending)
-
         best_ped_dist = dist_index[0] # Best pedestrian based on distance
-        best_ped_vel = vel_index[0] # Best pedestrian based on velocity
+        best_ped_vel = dist_index[0] # Best pedestrian based on velocity
 
-        # Select the best pedestrian:
+        # Robot navigation algorithm:
         if i > 0:
+            # Start by finding the best pedestrian:
             best_ped_smart = best_ped_dist
             for j in range(total_ped):
             # Loop through pedestrians from closest to furthest from target...
                 if vel[dist_index[j]] < 0:
                 # If their velocity is -ve (distance is decreasing)...
-                    best_ped_smart = dist_index[j]
-                    # They are the best pedestrian!!!
+                    best_ped_smart = dist_index[j] # They are the best pedestrian!!!
                     break
 
-            # Algorithm summary:
+            # Pedestrian selection summary:
             print("\nSELECTION:")
             print("- Based on distance: Ped %d @ %.1fm distance from target..." % (best_ped_dist, dist_sort[0]))
             print("- Based on velocity: Ped %d @ %.1fm/s velocity towards target..." % (best_ped_vel, -vel_sort[0]))
             print("- Based on combination: Ped %d @ minimum distance AND moving towards target..." % (best_ped_smart))
             best_ped = best_ped_smart
+
+            # Robot logic:
+            print("\nROBOT:")
+            odom = rospy.wait_for_message("/odometry/filtered", Odometry)
+            goal_xy = [ped.agent_states[best_ped].pose.position.x, ped.agent_states[best_ped].pose.position.y]
+            robot_xy = [odom.pose.pose.position.x, odom.pose.pose.position.y]
+            dist_robot_target = math.sqrt((robot_xy[0]-target_xy[0])**2+(robot_xy[1]-target_xy[1])**2)
+            dist_robot_ped = math.sqrt((robot_xy[0]-goal_xy[0])**2+(robot_xy[1]-goal_xy[1])**2)
+
+            # Make sure the best pedestrian is closer to target than last goal:
+            if dist[best_ped_smart] < dist_ped_target_last:
+                dist_ped_target_last = dist[best_ped_smart]
+                # If the best pedestrian is closer to target:
+                send_goal = 1
+                print("- Following: Ped %d @ %.1fm from pedestrian and %.1fm from target..." % (best_ped, dist_robot_ped, dist_robot_target))
+            else:
+                # If the best pedestrian is further away, don't send a navigaiton goal:
+                send_goal = 0
+                print("- Waiting for a pedestrian closer to the target. Robot @ %.fm from target... " % dist_robot_target)
+            if dist_robot_target < 2:
+                reached_target = 1
         else:
-            # For the very first iteration, just select closest pedestrian to target...
-            best_ped = best_ped_dist
             print("- Please wait for the next iteration...")
 
-        # Calculate distance between robot and best pedestrian:
-        goal_xy = [ped.agent_states[best_ped].pose.position.x, ped.agent_states[best_ped].pose.position.y]
-        odom = rospy.wait_for_message("/odometry/filtered", Odometry)
-        odom_xy = [odom.pose.pose.position.x, odom.pose.pose.position.y]
-        dist_robot_ped = math.sqrt((odom_xy[0]-goal_xy[0])**2+(odom_xy[1]-goal_xy[1])**2)
-        dist_robot_target = math.sqrt((odom_xy[0]-target_xy[0])**2+(odom_xy[0]-target_xy[1])**2)
-        print("\nROBOT:")
-        print("- Following: Ped %d @ %.1fm from pedestrian and %.1fm from target..." % (best_ped, dist_robot_ped, dist_robot_target))
-        print("----------------------------------------------------------")
-
         # Send navigation goal to navigation stack:
-        if dist_robot_target > 5:
+        if send_goal == 1:
             goal = MoveBaseGoal()
             goal.target_pose.header.frame_id = "odom"
             goal.target_pose.header.stamp = rospy.Time.now()
@@ -96,6 +107,10 @@ def movebase_client():
             goal.target_pose.pose.position.y = goal_xy[1]
             goal.target_pose.pose.orientation.w = 1.0
             client.send_goal(goal)
+        if reached_target == 1:
+            print("- Robot navigation complete!")
+            break
+        print("----------------------------------------------------------")
         time.sleep(t_delay)
 
 # If the python node is executed as main process (sourced directly)
@@ -104,8 +119,5 @@ if __name__ == '__main__':
        # Initializes a rospy node to let the SimpleActionClient publish and subscribe
         rospy.init_node('movebase_client_py')
         result = movebase_client()
-        #if result:
-        #    rospy.loginfo("Goal execution done!")
     except rospy.ROSInterruptException:
-        print("Finished.")
-        #rospy.loginfo("Navigation test finished.")
+        print("Algorithm finished!")
