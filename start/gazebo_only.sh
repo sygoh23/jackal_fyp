61dea9c5532ed0ccb67126e59ce43d44987e9936
Samuel <sygoh23@student.monash.edu>
2020-05-31 00:26:28 +1000

    Gmapping test.

--- start/gazebo_only.sh
+++
@@ -1,7 +0,0 @@
-# Launches Jackal simulation with pedestrians and gazebo integration...
-sudo killall rosmaster
-sudo killall gzserver
-sudo killall gzclient
-source /opt/ros/melodic/setup.bash
-source ../devel/setup.bash
-roslaunch development gazebo_only.launch
