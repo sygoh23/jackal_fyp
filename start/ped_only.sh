c93c97cd2849ad75e6966a94f8265d03d2d6c890
Samuel <sygoh23@student.monash.edu>
2020-05-31 00:08:09 +1000

    File system restructure.

--- start/ped_only.sh
+++
@@ -1,7 +0,0 @@
-# Launches pedestrian simulator only...
-sudo killall rosmaster
-sudo killall gzserver
-sudo killall gzclient
-source /opt/ros/melodic/setup.bash
-source ../devel/setup.bash
-roslaunch development ped_only.launch
