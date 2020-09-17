85f759316335fc7dff678b77a17b9450b6352467
Samuel <sygoh23@student.monash.edu>
2020-09-17 10:56:02 +1000

    Working line of sight detection (separate env).

--- src/development/scripts/old/line_of_sight_v2.py
+++ src/development/scripts/old/line_of_sight_v2.py
@@ -29,19 +29,9 @@ def extract_y(list):
 def extract_z(list):
     return [item[2] for item in list]
 
-def linear_const(x1, x2, y1, y2):
-        return (y1-y2), (x2-x1), (x1*y2-x2*y1)
-
-def linear_dist(A, B, C, x, y):
-        return abs(A*x+B*y+C)/sqrt(A**2+B**2)
-
-def inside_radius(x0, y0, r, x_in, y_in):
-        return (x_in-x0)**2+(y_in-y0)**2<=r**2
-
 robot_range = 75
-ped_tol = 1.5
-los_dev = 3
-z_min = 0.1
+ped_tol = -1
+z_min = 1
 z_max = 2
 while True:
     rospy.init_node('los_detection', anonymous=True)
@@ -49,7 +39,6 @@ while True:
     # Transform point cloud data:
     tf_buffer = tf2_ros.Buffer()
     tf_listener = tf2_ros.TransformListener(tf_buffer)
-    time.sleep(0.05)
     data = rospy.wait_for_message("/velodyne_points2", PointCloud2)
     transform = tf_buffer.lookup_transform('odom', 'velodyne2', rospy.Time(0))
     data_odom = do_transform_cloud(data, transform)
@@ -82,13 +71,13 @@ while True:
             ped_num_in_range.append(j)
 
     # Line of sight detection:
-    ped_x_los = []; ped_y_los = []; ped_num_los = []
+    ped_x_los = []; ped_y_los = [];
     print("\nRESULTS:")
     for k in range(len(ped_x_in_range)):
         current_ped_x = ped_x_in_range[k];
         current_ped_y = ped_y_in_range[k];
-        current_ped_num = ped_num_in_range[k];
 
+        # Insane filtering...
         # Check to make sure the point cloud points fall between the robot
         # and the pedestrian...
         export_point = False
@@ -113,43 +102,18 @@ while True:
             if export_point == True:
                 cloud_x_in_boundary.append(current_ped_x)
                 cloud_y_in_boundary.append(current_ped_y)
+        print("Pedestrian %d | Score: %d / %d" % (ped_num_in_range[k], len(cloud_x_in_boundary), len(cloud_x)))
 
-        a1 = ped_num_in_range[k]
-        a2 = len(cloud_x_in_boundary)
-        a3 = len(cloud_x)
-
-        allow_pedestrian = True
-        for j in range(len(cloud_x_in_boundary)):
-            ABC = linear_const(robot[0], current_ped_x, robot[1], current_ped_y)
-            dist = linear_dist(ABC[0], ABC[1], ABC[2], cloud_x_in_boundary[j], cloud_y_in_boundary[j])
-            if dist < los_dev:
-                allow_pedestrian = False
-
-        if (allow_pedestrian == True):
+        if (len(cloud_x_in_boundary) < 10):
             ped_x_los.append(current_ped_x)
             ped_y_los.append(current_ped_y)
-            ped_num_los.append(current_ped_num)
-        #print("- Pedestrian %d | Boundary Score: %d / %d | LOS: %d" % (a1, a2, a3, allow_pedestrian))
-
-    ped_x_excl = ped_x_all[:]
-    ped_y_excl = ped_y_all[:]
-    for k in reversed(ped_num_los):
-        del ped_x_excl[k]
-        del ped_y_excl[k]
-
-    print("- Total Peds: %d" % len(ped_x_all))
-    print("- Allowed Peds: %d" % len(ped_x_los))
-    print("- Excluded Peds: %d" % len(ped_x_excl))
 
     # Plotting the image:
-    plt.grid(b=True, which='major', color='#d6d6d6', linestyle='--')
     plt.scatter(robot[0], robot[1], c='b', marker='*', s=50)
-    plt.scatter(cloud_x, cloud_y, c='dimgray', marker='.', s=10)
-    plt.scatter(ped_x_excl, ped_y_excl, c='r', marker='.', s=20)
-    plt.scatter(ped_x_los, ped_y_los, c='g', marker='.', s=30)
-    plt.xlim((robot[0]-75, robot[0]+75))
-    plt.ylim((robot[1]-75, robot[1]+75))
+    plt.scatter(cloud_x, cloud_y, c='dimgray', marker='.', s=5)
+    plt.scatter(ped_x_all, ped_y_all, c='r', marker='.', s=10)
+    plt.scatter(ped_x_los, ped_y_los, c='g', marker='.', s=25)
     plt.gca().set_aspect('equal', adjustable='box')
     plt.savefig("/home/ubuntu/LOS.png")
     plt.clf()
-    time.sleep(2)
+    time.sleep(5)
